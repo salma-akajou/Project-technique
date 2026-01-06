a172517a5d822be805b1117a05c159acb5d2349f
salma akg <salmaakagou1@gmail.com>
2026-01-06 11:40:37 +0100

    feat: ajout de FilmService pour gérer la logique des films

---
+++ Films/app/Services/FilmService.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Services;
+use App\Models\Film;
+
+class FilmService
+{
+    public function getAll(array $filters = []){
+        $query= Film::query();
+        //rechercher par titre
+        if(!empty($filters['search'])){
+            $query->where('titre','like', '%' .$filters['search'] . '%');
+        }
+        
+        //filtrer par categories
+        if(!empty($filters['categorie_id'])){
+            $query->whereHas('categories', function($q) use ($filters){
+                $q->where('categorie_id', $filters['categorie_id']);
+            });
+        }
+        //pagination
+        return $query->paginate(5);
+    }
+
+    public function create(array $data){
+        //Upload image
+        if(!empty($data['image'])){
+            $data['image']= $data['image']->store('films', 'public');
+        }
+
+        $film= Film::create($data);
+
+        //associer les categories
+        if(!empty($data['categories'])){
+            $film->categories()->sync($data['categories']);
+        }
+
+        return $film;
+    }
+
+    public function update(Film $film, array $data){
+        if(!empty($data['image'])){
+            $data['image']= $data['image']->store('films', 'public');
+        }
+
+        $film->update($data);
+
+        if(!empty($data['categories'])){
+            $film->categories()->sync($data['categories']);
+        }
+    }
+
+    public function delete(Film $film){
+        $film->delete();
+    }
+
+}
